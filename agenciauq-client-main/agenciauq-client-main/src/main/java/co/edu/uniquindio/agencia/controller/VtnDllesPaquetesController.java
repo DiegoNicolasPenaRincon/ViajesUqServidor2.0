f25c41326ebb9a0eae458749bb9d30cdea1f43f1
Santiagobv687 <santiagobv687@gmail.com>
2023-11-18 22:49:47 -0500

    detalles del paquete Completado

--- agenciauq-client-main/agenciauq-client-main/src/main/java/co/edu/uniquindio/agencia/controller/VtnDllesPaquetesController.java
+++ agenciauq-client-main/agenciauq-client-main/src/main/java/co/edu/uniquindio/agencia/controller/VtnDllesPaquetesController.java
@@ -1,16 +1,37 @@
 package co.edu.uniquindio.agencia.controller;
 
+import co.edu.uniquindio.agencia.model.AgenciaViajes;
 import co.edu.uniquindio.agencia.model.Destino;
 import co.edu.uniquindio.agencia.model.PaqueteTuristico;
+import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VtnDllesPaquetesController {
+
+    @FXML
+    private ImageView imageView1;
+
+    @FXML
+    private ImageView imageView2;
+
+    @FXML
+    private ImageView imageView3;
+
+
+    private final AgenciaViajes agenciaViajes=AgenciaViajes.getInstance();
 
     @FXML
     private TableView<Destino> Destino;
@@ -28,7 +49,7 @@ public class VtnDllesPaquetesController {
     private TableColumn<Destino, String> colDescripcion;
 
     @FXML
-        private TableColumn<PaqueteTuristico, String> colNombre;
+        private TableColumn<Destino, String> colNombre;
 
     @FXML
     private Label lblCaracteristicas;
@@ -48,9 +69,10 @@ public class VtnDllesPaquetesController {
     private AnchorPane panelVentana;
     private PaqueteTuristico paqueteSeleccionado;
 
-    public void init(AnchorPane panelVentana, PaqueteTuristico paqueteSeleccionado){
-        this.panelVentana=panelVentana;
-        this.paqueteSeleccionado=paqueteSeleccionado;
+    public void init(AnchorPane panelVentana, PaqueteTuristico paqueteSeleccionado) {
+        this.panelVentana = panelVentana;
+        this.paqueteSeleccionado = paqueteSeleccionado;
+
 
         lblNombre.setText(paqueteSeleccionado.getNombre());
         lblCaracteristicas.setText(paqueteSeleccionado.getServicios());
@@ -58,6 +80,86 @@ public class VtnDllesPaquetesController {
         lblFinal.setText(paqueteSeleccionado.getFechaFin().toString());
         lblPrecio.setText(String.valueOf(paqueteSeleccionado.getPrecio()));
 
+        colNombre.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNombre()));
+        colCiudad.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getCiudad()));
+        colClima.setCellValueFactory(cellData ->
+                new SimpleStringProperty(cellData.getValue().getClima().name()));
+
+        ObservableList<Destino> listaDestinos = FXCollections.observableArrayList(paqueteSeleccionado.getDestinos());
+        Destino.setItems(listaDestinos);
+
+        Destino.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                // Cambiar las imágenes en los ImageView con las fotos del destino seleccionado
+                new Thread(() -> {
+                    actualizarImagenesDestino(newValue);
+                }).start();
+            }
+        });
+    }
+
+    private void actualizarImagenesDestino(Destino destinoSeleccionado) {
+       try{
+        // Obtener el ArrayList de fotos del destino
+        List<String> fotosDestino = destinoSeleccionado.getRutasImagenes();
+
+        // Actualizar las imágenes en los ImageView con las nuevas fotos
+        for (int i = 0; i < fotosDestino.size(); i++) {
+            String urlFoto = fotosDestino.get(i);
+            // Obtener el ImageView correspondiente al índice
+            ImageView imageView = obtenerImageViewPorIndice(i + 1);
+            if (imageView != null) {
+                // Configurar la nueva imagen en el ImageView
+                Image nuevaImagen = new Image(urlFoto);
+                Platform.runLater(() -> {
+                    imageView.setImage(nuevaImagen);
+                });
+            }
+        }
+    }catch(IllegalArgumentException e){
+           Platform.runLater(() -> {
+               Alert alert = new Alert(Alert.AlertType.ERROR);
+               alert.setContentText("Imagenes no disponibles en este momento.");
+               alert.setHeaderText(null);
+               alert.show();
+           });
+        }
+    }
+    private ImageView obtenerImageViewPorIndice(int indice) {
+        switch (indice) {
+            case 1:
+                return imageView1;
+            case 2:
+                return imageView2;
+            case 3:
+                return imageView3;
+            default:
+                return null;
+        }
+    }
+
+    public void verImagenes(){
+        Destino destinoSeleccionado=Destino.getSelectionModel().getSelectedItem();
+        ArrayList<String> imagenes=new ArrayList<>();
+        imagenes.addAll(destinoSeleccionado.getRutasImagenes());
 
     }
-}
+
+    public void regresar(){
+            try {
+                // Cargar el FXML de la nueva ventana
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/Ventanas/VtnPtesClientes.fxml"));
+                Pane nuevaVentana = loader.load();
+                VtnPtesClienteController ptesClienteController=loader.getController();
+                ptesClienteController.init(panelVentana);
+
+
+                // Limpiar el contenido anterior y establecer el nuevo contenido
+                panelVentana.getChildren().clear();
+                panelVentana.getChildren().add(nuevaVentana);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+

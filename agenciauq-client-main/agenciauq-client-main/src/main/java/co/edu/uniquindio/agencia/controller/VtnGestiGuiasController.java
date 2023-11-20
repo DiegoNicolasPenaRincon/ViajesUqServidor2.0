8210b0977c22218da7f4ecec79f7963c38945c7a
Diego Nicolas Peña Rincon <diegonicolaspenarincon@gmail.com>
2023-11-20 18:35:30 -0500

    mas cambios a todo lo relacionado con guias

--- agenciauq-client-main/agenciauq-client-main/src/main/java/co/edu/uniquindio/agencia/controller/VtnGestiGuiasController.java
+++ agenciauq-client-main/agenciauq-client-main/src/main/java/co/edu/uniquindio/agencia/controller/VtnGestiGuiasController.java
@@ -16,23 +16,6 @@ public class VtnGestiGuiasController {
 
     private AnchorPane panel;
 
-    public void mostrarVentanaAgregarGuias() {
-        try
-        {
-            // Cargar el FXML en el AnchorPane derecho
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Ventanas/VtnAgGuias.fxml"));
-            Pane nuevaVentana = loader.load();
-
-            // Limpiar el contenido anterior y establecer el nuevo contenido
-            panel.getChildren().clear();
-            panel.getChildren().add(nuevaVentana);
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
     public void abrirAgregarGuias(){
         try {
             // Cargar el FXML en el AnchorPane derecho
